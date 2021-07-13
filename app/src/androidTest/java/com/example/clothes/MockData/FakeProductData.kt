c84d4fed92fbb54b9bc297a224f9ff93f94c2f60
Carolina Maciel <carolinamaciel37@gmail.com>
2021-07-13 00:56:12 -0300

    Create a mock api response and create test of snapshot

---
+++ app/src/androidTest/java/com/example/clothes/MockData/FakeProductData.kt
@@ -0,0 +1,182 @@
+package com.example.clothes.MockData
+
+import com.example.clothes.model.Product
+import com.example.clothes.model.ProductSize
+
+class FakeProductData {
+    companion object {
+        val products: Array<Product> = arrayOf(
+            Product("https://d3l7rqep7l31az.cloudfront.net/images/products/20002605_615_catalog_1.jpg?1460136912",
+                "VESTIDO TRANSPASSE BOW",
+                "199,90 R$",
+                "",
+                false,
+                "3 x R$ 66,63 ",
+                "",
+                Array(1) {
+                    ProductSize(
+                        false,
+                        "P,M,G",
+                        "123"
+                    )
+                }),
+            Product("https://d3l7rqep7l31az.cloudfront.net/images/products/20002605_615_catalog_1.jpg?1460136912",
+                "REGATA ALCINHA FOLK",
+                "99,90 R$",
+                "",
+                false,
+                "3 x R$ 33,30 ",
+                "",
+                Array(1) {
+                    ProductSize(
+                        false,
+                        "P,M,G",
+                        "123"
+                    )
+                }),
+            Product("", "TOP CROPPED SUEDE", "99,90 R$", "",
+                false, "3 x R$ 33,30 ", "", Array(1) {
+                    ProductSize(
+                        false,
+                        "P,M,G",
+                        "123"
+                    )
+                }),
+            Product("", "BATA DECOTE FLUID", "99,90 R$", "",
+                false, "3 x R$ 33,30 ", "", Array(1) {
+                    ProductSize(
+                        false,
+                        "P,M,G",
+                        "123"
+                    )
+                }),
+            Product("", "T-SHIRT LEATHER DULL", "99,90 R$", "",
+                false, "3 x R$ 33,30 ", "", Array(1) {
+                    ProductSize(
+                        false,
+                        "P,M,G",
+                        "123"
+                    )
+                }),  Product("", "CAMISA SUEDE SPAN", "99,90 R$", "",
+                false, "3 x R$ 33,30 ", "", Array(1) {
+                    ProductSize(
+                        false,
+                        "P,M,G",
+                        "123"
+                    )
+                }), Product("", "CALÇA CLASSIC PRINT", "99,90 R$", "",
+                false, "3 x R$ 33,30 ", "", Array(1) {
+                    ProductSize(
+                        false,
+                        "P,M,G",
+                        "123"
+                    )
+                }),Product("", "REGATA ALCINHA FOLK", "99,90 R$", "",
+                false, "3 x R$ 33,30 ", "", Array(1) {
+                    ProductSize(
+                        false,
+                        "P,M,G",
+                        "123"
+                    )
+                }),Product("", "MACAQUINHO ALFAIATARIA PEACE", "99,90 R$", "",
+                false, "3 x R$ 33,30 ", "", Array(1) {
+                    ProductSize(
+                        false,
+                        "P,M,G",
+                        "123"
+                    )
+                }),Product("", "BOLSA FLAP TRIANGLE", "99,90 R$", "",
+                false, "3 x R$ 33,30 ", "", Array(1) {
+                    ProductSize(
+                        false,
+                        "P,M,G",
+                        "123"
+                    )
+                }),Product("", "VESTIDO LONGO FLOAT", "99,90 R$", "",
+                false, "3 x R$ 33,30 ", "", Array(1) {
+                    ProductSize(
+                        false,
+                        "P,M,G",
+                        "123"
+                    )
+                }),Product("", "BATA DECOTE FLUID", "99,90 R$", "",
+                false, "3 x R$ 33,30 ", "", Array(1) {
+                    ProductSize(
+                        false,
+                        "P,M,G",
+                        "123"
+                    )
+                }),Product("", "VESTIDO FRANZIDO RECORTES", "99,90 R$", "",
+                false, "3 x R$ 33,30 ", "", Array(1) {
+                    ProductSize(
+                        false,
+                        "P,M,G",
+                        "123"
+                    )
+                }),Product("", "CALÇA COMFORT TASSEL", "99,90 R$", "",
+                false, "3 x R$ 33,30 ", "", Array(1) {
+                    ProductSize(
+                        false,
+                        "P,M,G",
+                        "123"
+                    )
+                }),Product("", "SAIA DENIM BOTÕES", "99,90 R$", "",
+                false, "3 x R$ 33,30 ", "", Array(1) {
+                    ProductSize(
+                        false,
+                        "P,M,G",
+                        "123"
+                    )
+                }),Product("", "BLUSA LAÇO ISTAMBUL", "99,90 R$", "",
+                false, "3 x R$ 33,30 ", "", Array(1) {
+                    ProductSize(
+                        false,
+                        "P,M,G",
+                        "123"
+                    )
+                }),Product("", "BLUSA RIB STRIPES", "99,90 R$", "",
+                false, "3 x R$ 33,30 ", "", Array(1) {
+                    ProductSize(
+                        false,
+                        "P,M,G",
+                        "123"
+                    )
+                }),Product("", "CASACO WHITE FUR", "99,90 R$", "",
+                false, "3 x R$ 33,30 ", "", Array(1) {
+                    ProductSize(
+                        false,
+                        "P,M,G",
+                        "123"
+                    )
+                }),Product("", "VESTIDO CURTO FESTIVAL", "99,90 R$", "",
+                false, "3 x R$ 33,30 ", "", Array(1) {
+                    ProductSize(
+                        false,
+                        "P,M,G",
+                        "123"
+                    )
+                }),Product("", "ÓCULOS DE SOL BOLD", "99,90 R$", "",
+                false, "3 x R$ 33,30 ", "", Array(1) {
+                    ProductSize(
+                        false,
+                        "P,M,G",
+                        "123"
+                    )
+                }),Product("", "ÓCULOS DE SOL AVIADOR VINTAGE", "99,90 R$", "",
+                false, "3 x R$ 33,30 ", "", Array(1) {
+                    ProductSize(
+                        false,
+                        "P,M,G",
+                        "123"
+                    )
+                }),Product("", "PULSEIRA STYLISH", "99,90 R$", "",
+                false, "3 x R$ 33,30 ", "", Array(1) {
+                    ProductSize(
+                        false,
+                        "P,M,G",
+                        "123"
+                    )
+                }),
+        )
+    }
+}
--- app/src/androidTest/java/com/example/clothes/ProductListActivityTest.kt
+++ app/src/androidTest/java/com/example/clothes/ProductListActivityTest.kt
@@ -1,6 +1,7 @@
 package com.example.clothes
 
 import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.Espresso.pressBack
 import androidx.test.espresso.action.ViewActions.click
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.contrib.RecyclerViewActions
@@ -8,6 +9,7 @@ import androidx.test.espresso.contrib.RecyclerViewActions.actionOnItemAtPosition
 import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.rule.ActivityTestRule
+import com.example.clothes.MockData.FakeProductData
 import com.example.clothes.ui.activity.ProductListActivity
 import com.example.clothes.ui.adapter.ProductsListAdapter
 import org.junit.Rule
@@ -18,22 +20,27 @@ import org.junit.runner.RunWith
 class ProductListActivityTest {
     @get:Rule
     val rule = ActivityTestRule(ProductListActivity::class.java)
-
-    val CLOTHES_NAME_TEST = listOf("pulseira")
+    val productsListMock = FakeProductData.products
 
     @Test
-    fun shouldLoadProductsInRecyclerView(){
+    fun shouldLoadProductsInRecyclerView_whenHasProducts(){
         onView(withId(R.id.clothes_recyclerview)).perform(click()).check(matches(isDisplayed()))
     }
 
-//    @Test
-//    fun shouldClickInProductCardView(){
-//        onView(withId(R.id.clothes_recyclerview)).perform(actionOnItemAtPosition<ProductsListAdapter.ViewHolder>(0, click()))
-//
-//        onView(withId(R.id.clothes_title)).check(matches(withText(CLOTHES_NAME_TEST)))
-//
-//        pressBack()
-//
-//        onView(withId(R.id.recycler_view)).check(matches(isDisplayed()))
-//    }
+
+    //To test pass do you need disable animations on emulator or physical device
+    @Test
+    fun shouldClickInProductCardView_whenHasProduct(){
+        onView(withId(R.id.clothes_recyclerview)).perform(actionOnItemAtPosition<ProductsListAdapter.ViewHolder>(0, click()))
+        onView(withId(R.id.name_product)).check(matches(withText(productsListMock[0].name)))
+        pressBack()
+        onView(withId(R.id.clothes_recyclerview)).check(matches(isDisplayed()))
+    }
+
+    @Test
+    fun shouldScrollToFindTheProduct_whenHasProductInPosition(){
+        onView(withId(R.id.clothes_recyclerview)).perform(RecyclerViewActions.scrollToPosition<ProductsListAdapter.ViewHolder>(21))
+        onView(withId(R.id.clothes_recyclerview)).perform(actionOnItemAtPosition<ProductsListAdapter.ViewHolder>(21, click()))
+        onView(withText(productsListMock[21].name)).check(matches(isDisplayed()))
+    }
 }
